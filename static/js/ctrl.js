f980ad2092bcab6625b33fe79eaf8fb9109a925f
Elcarlos2925 <cl154204@gmail.com>
2024-05-01 19:05:09 -0700

    UI for song

--- static/js/ctrl.js
+++ static/js/ctrl.js
@@ -1,33 +1,52 @@
-let progress = document.getElementById("progress-song")
-let song = document.getElementById("song")
-let ctrl = document.getElementById("ctrl-id")
-song.onloadedmetadata = function(){
-    progress.max = song.duration
-    progress.value = song.currentTime
-}
+let progress = document.getElementById("progress-song");
+let song = document.getElementById("song");
+let ctrl = document.getElementById("ctrl-id");
+let currentTimeDisplay = document.getElementById("current-time");
+let totalTimeDisplay = document.getElementById("total-time");
+let interval;
 
-function playPause(){
-    if(ctrl.classList.contains("fa-pause")){
-        song.pause()
-        ctrl.classList.remove("fa-pause")
-        ctrl.classList.add("fa-play")
+song.onloadedmetadata = function() {
+    progress.max = song.duration;
+    progress.value = song.currentTime;
+    totalTimeDisplay.textContent = formatTime(song.duration);
+}
+function playPause() {
+    if (ctrl.classList.contains("fa-pause")) {
+        song.pause();
+        ctrl.classList.remove("fa-pause");
+        ctrl.classList.add("fa-play");
+        clearInterval(interval);
+    } else {
+        song.play();
+        ctrl.classList.add("fa-pause");
+        ctrl.classList.remove("fa-play");
         interval = setInterval(() => {
             progress.value = song.currentTime;
+            currentTimeDisplay.textContent = formatTime(song.currentTime);
         }, 500);
-    } else{
-        song.play()
-        ctrl.classList.add("fa-pause")
-        ctrl.classList.remove("fa-play")
     }
 }
 
-function playInterval(){
-    song.play()
-    ctrl.classList.add("fa-pause")
-    ctrl.classList.remove("fa-play")
+function playInterval() {
+    song.play();
+    ctrl.classList.add("fa-pause");
+    ctrl.classList.remove("fa-play");
+    interval = setInterval(() => {
+        progress.value = song.currentTime;
+        currentTimeDisplay.textContent = formatTime(song.currentTime);
+    }, 500);
 }
 
-progress.onchange = function(){
-    song.play()
-    song.currentTime = progress.value
+progress.onchange = function() {
+    song.play();
+    song.currentTime = progress.value;
+}
+
+function formatTime(seconds) {
+    let minutes = Math.floor(seconds / 60);
+    let secs = Math.floor(seconds % 60);
+    if (secs < 10) {
+        secs = "0" + secs;
+    }
+    return minutes + ":" + secs;
 }
