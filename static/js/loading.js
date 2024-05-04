013e6a2362766647d9238c81123da78f42506115
Elcarlos2925 <cl154204@gmail.com>
2024-05-04 14:31:22 -0700

    forward and backward buttons

--- static/js/loading.js
+++ static/js/loading.js
@@ -1,27 +1,58 @@
-fetch('../static/js/songs.json')
+songListened = []
+let random
+
+function generateRandomNumber(){
+    do{
+        random = Math.floor(Math.random() * 39)
+    } while (songListened.includes(random))
+    songListened.push(random)
+    clearSongListened()
+    reload()
+    return random
+}
+function backward() {
+    if (songListened.length > 1) {
+        songListened.pop();
+        reload()
+        clearSongListened()
+        random = songListened[songListened.length - 1];
+    } else {
+        return null; // No hay números anteriores
+    }
+}
+
+function clearSongListened(){
+    if (songListened.length >= 39){
+        songListened = []
+    }
+}
+function reload(){
+    fetch('../static/js/songs.json')
   .then(response => response.json())
   .then(songs => {
 
     // Aquí puedes usar los datos cargados del archivo JSON para actualizar el contenido de tu página.
     document.getElementById("img").innerHTML = `
-        <img src='${songs[23].Img}' alt='${songs[23].Title}' class='img-song'>
+        <img src='${songs[random].Img}' alt='${songs[random].Title}' class='img-song'>
     `;
     
     document.getElementById("title").innerHTML = `
-        <h1 class='title'>${songs[23].Title}</h1>
+        <h1 class='title'>${songs[random].Title}</h1>
     `;
     
     document.getElementById("artist").innerHTML = `
-        <p class='artist'>${songs[23].Artist}</p>
+        <p class='artist'>${songs[random].Artist}</p>
     `;
 
     let sourceSong = document.getElementById('song');
-    sourceSong.src = songs[23].Url;
-    console.log(sourceSong.src)
-
+    sourceSong.src = songs[random].Url;
    
 
   })
   .catch(error => {
     console.error('Error al cargar el archivo JSON:', error);
   });
+}
+
+generateRandomNumber()
+reload()
