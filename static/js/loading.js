ce1cb8034b828b2e2ab10a994ded378bb4277f59
Elcarlos2925 <cl154204@gmail.com>
2024-10-01 17:26:44 -0700

    test

--- static/js/loading.js
+++ static/js/loading.js
@@ -3,7 +3,7 @@ let random
 
 function forward(){
     do{
-        random = Math.floor(Math.random() * 92)
+        random = Math.floor(Math.random() * 1)
     } while (songListened.includes(random))
     songListened.push(random)
     clearSongListened()
@@ -19,7 +19,7 @@ function backward() {
 }
 
 function clearSongListened(){
-    if (songListened.length >= 92){
+    if (songListened.length >= 1){
         songListened = []
     }
 }
